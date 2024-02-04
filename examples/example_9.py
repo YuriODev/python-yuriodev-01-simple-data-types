04401b7fc1fcac93156a237b47173d41ed92f166
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-02-04 19:43:19 +0000

    Update example_9.py

--- examples/example_9.py
+++ examples/example_9.py
@@ -1,2 +1,8 @@
-# Example 9
-# Some example Python code here
+# Read the input number from the user
+n = int(input("Enter the number of natural numbers to sum: "))
+
+# Calculate the sum of the first n natural numbers
+sum_of_numbers = n * (n + 1) // 2
+
+# Print the result
+print(sum_of_numbers)
