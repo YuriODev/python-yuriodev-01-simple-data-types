27c76d37d434ff98edd75140b2d1f3d5e7da2401
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-02-04 18:22:39 +0000

    Update example_1.py

--- examples/example_1.py
+++ examples/example_1.py
@@ -1,2 +1,8 @@
-# Example 1
-# Some example Python code here
+# Read the input number from the user
+number = int(input("Enter a natural number: "))
+
+# Get ten place digit using integer division and modulo
+tens_place = (number // 10) % 10
+
+# Print the result using f-string
+print(f"The tens place digit of {number} is {tens_place}")
