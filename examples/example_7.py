c551752f952a098d6b8aea05c8d8e5502747d3ff
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-02-04 19:35:59 +0000

    Update example_7.py

--- examples/example_7.py
+++ examples/example_7.py
@@ -1,2 +1,12 @@
-# Example 7
-# Some example Python code here
+# Read the input numbers from the user
+n = int(input("Enter the integer: "))
+k = int(input("Enter the number of digits to cut off: "))
+
+# Calculate the divisor
+divisor = 10**k
+
+# Calculate the result
+result = n // divisor
+
+# Print the result
+print(result)
