f4acfcc16bf491bebfcbd14bfb71b680d9136f0f
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-02-04 20:35:05 +0000

    Update test_exercise_2.py

--- exercises/test_exercise_2.py
+++ exercises/test_exercise_2.py
@@ -2,21 +2,21 @@ import unittest
 import subprocess
 
 
-class TestExample2(unittest.TestCase):
-    def test_output_153(self):
-        # Test when the input is 153
-        output = subprocess.check_output(['python3', 'example_2.py'], input='153\n', text=True)
-        self.assertIn('The sum of the digits of 153 is 9', output)
+class TestExample1(unittest.TestCase):
+    def test_output_1981(self):
+        # Test when the input is 1981
+        output = subprocess.check_output(['python3', 'example_1.py'], input='1981\n', text=True)
+        self.assertIn('The tens place digit of 1981 is 8', output)
 
-    def test_output_123(self):
-        # Test when the input is 123
-        output = subprocess.check_output(['python3', 'example_2.py'], input='123\n', text=True)
-        self.assertIn('The sum of the digits of 123 is 6', output)
+    def test_output_158(self):
+        # Test when the input is 158
+        output = subprocess.check_output(['python3', 'example_1.py'], input='158\n', text=True)
+        self.assertIn('The tens place digit of 158 is 5', output)
 
-    def test_output_565(self):
-        # Test when the input is 565
-        output = subprocess.check_output(['python3', 'example_2.py'], input='565\n', text=True)
-        self.assertIn('The sum of the digits of 565 is 16', output)
+    def test_output_5(self):
+        # Test when the input is 5
+        output = subprocess.check_output(['python3', 'example_1.py'], input='5\n', text=True)
+        self.assertIn('The tens place digit of 5 is 0', output)
 
 
 if __name__ == '__main__':
