1c996a49c3c8d22c17204e2f9b2d36dc7f44f161
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-02-04 20:35:07 +0000

    Update test_exercise_3.py

--- exercises/test_exercise_3.py
+++ exercises/test_exercise_3.py
@@ -1,23 +1,18 @@
 import unittest
 import subprocess
 
+class TestExercise3(unittest.TestCase):
+    def test_case_1(self):
+        output = subprocess.check_output(['python3', 'exercise3.py'], input='3602\n', text=True)
+        self.assertIn('1:00:02', output)
 
-class TestExample2(unittest.TestCase):
-    def test_output_153(self):
-        # Test when the input is 153
-        output = subprocess.check_output(['python3', 'example_2.py'], input='153\n', text=True)
-        self.assertIn('The sum of the digits of 153 is 9', output)
+    def test_case_2(self):
+        output = subprocess.check_output(['python3', 'exercise3.py'], input='4556789\n', text=True)
+        self.assertIn('17:46:29', output)
 
-    def test_output_123(self):
-        # Test when the input is 123
-        output = subprocess.check_output(['python3', 'example_2.py'], input='123\n', text=True)
-        self.assertIn('The sum of the digits of 123 is 6', output)
-
-    def test_output_565(self):
-        # Test when the input is 565
-        output = subprocess.check_output(['python3', 'example_2.py'], input='565\n', text=True)
-        self.assertIn('The sum of the digits of 565 is 16', output)
-
+    def test_case_3(self):
+        output = subprocess.check_output(['python3', 'exercise3.py'], input='4568\n', text=True)
+        self.assertIn('1:16:08', output)
 
 if __name__ == '__main__':
     unittest.main()
