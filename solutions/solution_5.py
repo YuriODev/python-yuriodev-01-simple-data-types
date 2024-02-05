bf01f5db8c3e08d80b05904dd1c544cbe49606e9
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-02-05 12:05:32 +0000

    Update solution_5.py

--- solutions/solution_5.py
+++ solutions/solution_5.py
@@ -1,2 +1,12 @@
-# Solution 5
-# Solution will be provided here later
+# Read the input
+a = int(input())
+b = int(input())
+
+# Calculate the maximum value
+max_value = (a + b + abs(a - b)) // 2
+
+# Alternative solution
+max_value = a * (a > b) + b * (b >= a)
+
+# Print the maximum value
+print(max_value)
