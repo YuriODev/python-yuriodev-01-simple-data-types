a8b4fe957fb6b18f2fcee78d110717cbb4677ba9
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-02-05 12:20:55 +0000

    Update exercise_9.py

--- exercises/exercise_9.py
+++ exercises/exercise_9.py
@@ -1,2 +1,10 @@
-# Exercise 9
-# Your solution comes here
+# Read the input
+h = int(input())
+m = int(input())
+s = int(input())
+
+# Calculate the angle
+angle = 0.5 * (60 * h + m + s / 60)
+
+# Print the angle
+print(angle)
