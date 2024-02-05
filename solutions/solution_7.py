016e4b9627a4a5a4035a92366bb1210a595c5869
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-02-05 12:14:26 +0000

    Update solution_7.py

--- solutions/solution_7.py
+++ solutions/solution_7.py
@@ -1,2 +1,14 @@
-# Solution 7
-# Solution will be provided here later
+# # Read the input
+n = int(input())
+
+# Extract the digits
+d1 = n // 1000
+d2 = (n % 1000) // 100
+d3 = (n % 100) // 10
+d4 = n % 10
+
+# Calculate the sum of the digits
+sum_of_digits = d1 + d2 + d3 + d4
+
+# Print the sum of the digits
+print(sum_of_digits)
