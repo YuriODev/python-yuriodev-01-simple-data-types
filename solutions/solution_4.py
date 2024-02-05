91075dd709a538b0c9afc76416bef6c78dc0f215
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-02-05 11:11:02 +0000

    Update solution_4.py

--- solutions/solution_4.py
+++ solutions/solution_4.py
@@ -1,2 +1,24 @@
-# Solution 4
-# Solution will be provided here later
+# Read the input
+n = int(input())
+
+# Extract the digits
+d1 = n // 1000
+d2 = (n % 1000) // 100
+d3 = (n % 100) // 10
+d4 = n % 10
+
+# # SOLUTION 1
+
+# # Calculate the result
+# result = (d1 == d4) * (d2 == d3)
+
+# # Print the result
+# print(result)
+
+# SOLUTION 2
+
+# Calculate the difference
+difference = abs((d1 - d4) + (d2 - d3))
+
+# Print the result
+print(max(1 - difference, 0))
