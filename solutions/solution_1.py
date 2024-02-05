dd723c3496c27c383b411296b69a9e7821aa427d
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-02-05 11:04:56 +0000

    Update solution_1.py

--- solutions/solution_1.py
+++ solutions/solution_1.py
@@ -1,2 +1,9 @@
-# Solution 1
-# Solution will be provided here later
+# Read the input number without printing a prompt
+number = int(input("Enter a five-digit number: "))
+
+# Calculate the sums and print the result
+first_part = (number // 10000) + ((number // 100) % 10) + (number % 10)
+print(first_part)
+second_part = ((number // 1000) % 10) + ((number // 10) % 10)
+
+print(str(first_part) + str(second_part))
