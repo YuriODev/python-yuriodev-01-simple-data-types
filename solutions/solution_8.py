baf2921f64da763af2868f45164a8881d581208b
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-02-05 12:17:56 +0000

    Update solution_8.py

--- solutions/solution_8.py
+++ solutions/solution_8.py
@@ -1,2 +1,14 @@
-# Solution 8
-# Solution will be provided here later
+# Read the input
+a = int(input())
+b = int(input())
+c = int(input())
+
+# Calculate the minimum, middle, and maximum values
+min_value = a * (a <= b and a <= c) + b * (b < a or b < c) + c * (c < a and c < b)
+max_value = a * (a >= b and a >= c) + b * (b > a or b > c) + c * (c > a and c > b)
+mid_value = a + b + c - min_value - max_value
+
+# Print the sorted numbers
+print(min_value)
+print(mid_value)
+print(max_value)
