f485b38461f9e11af0f730ac4d8958ae4ab0623e
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-02-06 10:21:29 +0000

    Update exercise_3.py

--- exercises/exercise_3.py
+++ exercises/exercise_3.py
@@ -1,9 +1,2 @@
-# Read the input number from the user
-t = int(input("Enter the number of minutes after midnight: "))
-
-# Calculate the hours and minutes
-hours = t // 60
-minutes = t % 60
-
-# Print the result using f-string
-print(f"{hours}\n{minutes}")
+# Exercise 3
+# Your solution comes here
