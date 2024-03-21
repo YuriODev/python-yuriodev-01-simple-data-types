193dee7d1f474abd97abec7f5af7a6e52f3888b6
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-03-21 09:34:10 +0000

    Update test_exercise_11.py

--- exercises/test_exercise_11.py
+++ exercises/test_exercise_11.py
@@ -14,27 +14,47 @@ class TestBillDenominations(unittest.TestCase):
     def test_example_1(self):
         """Test the provided example 1."""
         output = self.run_exercise("534\n")
-        self.assertEqual(output, "1 (500), 0 (100), 3 (10), 0 (5), 4 (1)\n")
+        self.assertIn("1 (500)", output)
+        self.assertIn("0 (100)", output)
+        self.assertIn("3 (10)", output)
+        self.assertIn("0 (5)", output)
+        self.assertIn("4 (1)", output)
 
     def test_example_2(self):
         """Test the provided example 2."""
         output = self.run_exercise("1245\n")
-        self.assertEqual(output, "2 (500), 2 (100), 4 (10), 1 (5), 0 (1)\n")
+        self.assertIn("2 (500)", output)
+        self.assertIn("2 (100)", output)
+        self.assertIn("4 (10)", output)
+        self.assertIn("1 (5)", output)
+        self.assertIn("0 (1)", output)
 
     def test_example_3(self):
         """Test the provided example 3."""
         output = self.run_exercise("12\n")
-        self.assertEqual(output, "0 (500), 0 (100), 1 (10), 0 (5), 2 (1)\n")
+        self.assertIn("0 (500)", output)
+        self.assertIn("0 (100)", output)
+        self.assertIn("1 (10)", output)
+        self.assertIn("0 (5)", output)
+        self.assertIn("2 (1)", output)
 
     def test_example_4(self):
         """Test the provided example 4."""
         output = self.run_exercise("786\n")
-        self.assertEqual(output, "1 (500), 2 (100), 8 (10), 1 (5), 1 (1)\n")
+        self.assertIn("1 (500)", output)
+        self.assertIn("2 (100)", output)
+        self.assertIn("8 (10)", output)
+        self.assertIn("1 (5)", output)
+        self.assertIn("1 (1)", output)
 
     def test_example_5(self):
         """Test the provided example 5."""
         output = self.run_exercise("377\n")
-        self.assertEqual(output, "0 (500), 3 (100), 7 (10), 1 (5), 2 (1)\n")
+        self.assertIn("0 (500)", output)
+        self.assertIn("3 (100)", output)
+        self.assertIn("7 (10)", output)
+        self.assertIn("1 (5)", output)
+        self.assertIn("2 (1)", output)
 
 
 
