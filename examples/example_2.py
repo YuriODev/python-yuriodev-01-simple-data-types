6d87e176bf89f8eb9e60b479f377b5b4a50be29d
Yurii Oksamytnyi <141469505+YuriiOks@users.noreply.github.com>
2024-05-08 13:59:17 +0100

    Update example_2.py

--- examples/example_2.py
+++ examples/example_2.py
@@ -2,6 +2,11 @@
 number = int(input("Enter a three-digit number: "))
 
 # Extract each digit
+
+# -- Explanation --
+# To extract the first digit in a number you need to divide by 10^(n-1) 
+# where n is the number of digits in the number
+
 hundreds = number // 100
 tens = (number // 10) % 10
 ones = number % 10
